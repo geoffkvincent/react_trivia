9af21ec81becea11d34bfa7abf9b1ed9abe6f6d1
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-20 16:45:44 -0700

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+@cat_arr = ['React-101', 'Advanced React', 'ES 2015', 'ES6']
+@correct_ans_arr = ['a', 'b', 'c', 'd']
+@points_arr = ['100', '200', '300', '400', '500']
+
+@cat_arr.each do |cat|
+  c = Category.create(
+    name: cat
+  )
+  5.times do
+    c.cards.create(
+      question: Faker::Hacker.say_something_smart,
+      answer_a: Faker::Hacker.adjective,
+      answer_b: Faker::Hacker.noun,
+      answer_c: Faker::Hacker.verb,
+      answer_d: Faker::Hacker.abbreviation,
+      correct_answer: @correct_ans_arr.sample,
+      points: @points_arr.sample
+      )
+  end
+end
+
+puts 'seeded'
