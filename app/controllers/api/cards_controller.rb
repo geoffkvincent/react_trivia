7df258df0b0ba3c812d4456f595ed8e348162e33
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-20 16:19:05 -0700

    card controller

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -1,16 +1,48 @@
 class Api::CardsController < ApplicationController
+  before_action set_category
+  before_action set_card, only: [:show, :update, :destroy]
+
   def index
+    render json: @category.cards
   end
 
   def show
+    render json: @category.card
   end
 
   def create
+    card = @category.card.new(card_params)
+
+    if card.save
+      render json: card
+    else
+      render json: { errors: card.errors }
+    end
   end
 
   def update
+    if @card.update(card_params)
+      render json: @card
+    else
+      render json: @card.errors
+    end
   end
 
   def destroy
+    @card.destroy
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:category_id])
+  end
+
+  def set_card
+    @card = Card.find(params[:id])
+  end
+
+  def card_params
+    params.require(:card).permit(:question :answer_a, :answer_b, :answer_c, :answer_d, :correct_answer, :points)
   end
 end
