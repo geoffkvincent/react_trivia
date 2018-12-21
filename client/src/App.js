75a9d55e3fcbb3f95c7b8a52235371fb8f7ee44e
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-21 08:22:11 -0700

    app routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom'
 import { Container } from 'semantic-ui-react'
+import GameBoard from './components/GameBoard'
 
 class App extends React.Component {
   render() {
     return (
      <Container>
+       <Switch>
+         <Route exact path='/react-trivia' component={GameBoard}/>
+         <Route render={() => <h1 className='text-center'>Four oh Four.</h1>} />
+       </Switch>
      </Container>
     );
   }
