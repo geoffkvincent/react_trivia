33ff7554df1fee94c481958a8327c08a0c86091d
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-21 11:08:19 -0700

    initial board layout

--- client/src/components/Category.js
+++ client/src/components/Category.js
@@ -7,72 +7,72 @@ class Category extends React.Component{
       <Container>
         <Card.Group itemsPerRow={4}>
           <Card>
-            <Card.Content><h1>100</h1></Card.Content>
+            <Card.Content textAlign='center'><h1>100</h1></Card.Content>
           </Card>
           <Card>
-            <Card.Content><h1>100</h1></Card.Content>
+            <Card.Content textAlign='center'><h1>100</h1></Card.Content>
           </Card>
           <Card>
-            <Card.Content><h1>100</h1></Card.Content>
+            <Card.Content textAlign='center'><h1>100</h1></Card.Content>
           </Card>
           <Card>
-            <Card.Content><h1>100</h1></Card.Content>
-          </Card>
-        </Card.Group>
-        <Card.Group itemsPerRow={4}>
-          <Card>
-            <Card.Content></Card.Content>
-          </Card>
-          <Card>
-            <Card.Content></Card.Content>
-          </Card>
-          <Card>
-            <Card.Content></Card.Content>
-          </Card>
-          <Card>
-            <Card.Content></Card.Content>
+            <Card.Content textAlign='center'><h1>100</h1></Card.Content>
           </Card>
         </Card.Group>
         <Card.Group itemsPerRow={4}>
           <Card>
-            <Card.Content></Card.Content>
+            <Card.Content><h1>200</h1></Card.Content>
           </Card>
           <Card>
-            <Card.Content></Card.Content>
+            <Card.Content><h1>200</h1></Card.Content>
           </Card>
           <Card>
-            <Card.Content></Card.Content>
+            <Card.Content><h1>200</h1></Card.Content>
           </Card>
           <Card>
-            <Card.Content></Card.Content>
-          </Card>
-        </Card.Group>
-        <Card.Group itemsPerRow={4}>
-          <Card>
-            <Card.Content></Card.Content>
-          </Card>
-          <Card>
-            <Card.Content></Card.Content>
-          </Card>
-          <Card>
-            <Card.Content></Card.Content>
-          </Card>
-          <Card>
-            <Card.Content></Card.Content>
+            <Card.Content><h1>200</h1></Card.Content>
           </Card>
         </Card.Group>
         <Card.Group itemsPerRow={4}>
           <Card>
-            <Card.Content></Card.Content>
+            <Card.Content><h1>300</h1></Card.Content>
           </Card>
           <Card>
-            <Card.Content></Card.Content>
+            <Card.Content><h1>300</h1></Card.Content>
           </Card>
           <Card>
-            <Card.Content></Card.Content>
+            <Card.Content><h1>300</h1></Card.Content>
           </Card>
           <Card>
-            <Card.Content></Card.Content>
+            <Card.Content><h1>300</h1></Card.Content>
+          </Card>
+        </Card.Group>
+        <Card.Group itemsPerRow={4}>
+          <Card>
+            <Card.Content><h1>400</h1></Card.Content>
+          </Card>
+          <Card>
+            <Card.Content><h1>400</h1></Card.Content>
+          </Card>
+          <Card>
+            <Card.Content><h1>400</h1></Card.Content>
+          </Card>
+          <Card>
+            <Card.Content><h1>400</h1></Card.Content>
+          </Card>
+        </Card.Group>
+        <Card.Group itemsPerRow={4}>
+          <Card>
+            <Card.Content><h1>500</h1></Card.Content>
+          </Card>
+          <Card>
+            <Card.Content><h1>500</h1></Card.Content>
+          </Card>
+          <Card>
+            <Card.Content><h1>500</h1></Card.Content>
+          </Card>
+          <Card>
+            <Card.Content><h1>500</h1></Card.Content>
           </Card>
         </Card.Group>
       </Container>
