a0fcac1fb5b1340b5b31dcc97240c684d7663cf1
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-23 15:35:42 -0500

    get cards

--- client/src/components/Categories.js
+++ client/src/components/Categories.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { getCats } from '../reducers/categories'
 import { Card } from 'semantic-ui-react'
-import Category from './Category'
 
 class Categories extends React.Component {
 
