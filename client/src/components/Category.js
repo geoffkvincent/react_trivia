df68f2bbf6bb808bbf5a22be3100a1f89d0f9308
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-27 13:21:04 -0700

    render cards

--- client/src/components/Category.js
+++ client/src/components/Category.js
@@ -15,7 +15,7 @@ class Category extends React.Component {
     const { cards } = this.props
     return (
       <Container>
-        <Card.Group itemsPerRow={4}>
+        <Card.Group itemsPerRow={1}>
           { cards.map(card => 
           <Card key={card.id}>
             <Modal trigger={
@@ -27,10 +27,10 @@ class Category extends React.Component {
               <Modal.Description>
                 <ul>
                   <h1>{card.question}</h1>
-                  <li>{card.answer_a}</li>
-                  <li>{card.answer_b}</li>
-                  <li>{card.answer_c}</li>
-                  <li>{card.answer_d}</li>
+                  <li><h2>A:{card.answer_a}</h2></li>
+                  <li><h2>B:{card.answer_b}</h2></li>
+                  <li><h2>C:{card.answer_c}</h2></li>
+                  <li><h2>D:{card.answer_d}</h2></li>
                 </ul>
               </Modal.Description>
             </Modal.Content>
