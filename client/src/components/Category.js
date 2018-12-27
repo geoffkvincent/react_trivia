539996f8d654b7040f24b07fc869857f10d94a66
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-27 12:53:52 -0700

    render cards

--- client/src/components/Category.js
+++ client/src/components/Category.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { getCards } from '../reducers/cards'
-import Cards from './Card'
+import GameCard from './Card'
 import { Card, Container, Modal } from 'semantic-ui-react'
 
 class Category extends React.Component {
@@ -12,20 +12,23 @@ class Category extends React.Component {
   }
 
   render() {
+    const { cards } = this.props
     return (
       <Container>
         <Card.Group itemsPerRow={4}>
+          { cards.map(card => 
           <Card>
             <Modal trigger={
-              <Card.Content textAlign='center'><h1>100</h1></Card.Content>
+              <Card.Content textAlign='center'><h1>{card.points}</h1></Card.Content>
             }>
             <Modal.Content>
               <Modal.Description>
-                <Cards />
+                <GameCard {...cards}/>
               </Modal.Description>
             </Modal.Content>
             </Modal>
           </Card>
+            )}
         </Card.Group>
       </Container>
     )
--- client/src/reducers/cards.js
+++ client/src/reducers/cards.js
@@ -3,18 +3,15 @@ import axios from 'axios'
 const CARDS = 'CARDS'
 
 export const getCards = (id) => {
-  debugger
   return (dispatch) => {
     axios.get(`/api/categories/${id}/cards`)
       .then( res => dispatch({ type: CARDS, cards: res.data }))
-      debugger
   }
 }
 
 export default (state = [], action) => {
   switch(action.type) {
     case CARDS:
-    debugger
       return action.cards
     default:
       return state
