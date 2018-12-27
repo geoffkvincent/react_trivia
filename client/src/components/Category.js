6de2b37371649ec9f642566342e84ffda5455240
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-27 15:32:11 -0700

    render cards

--- client/src/components/Category.js
+++ client/src/components/Category.js
@@ -4,10 +4,31 @@ import { getCards } from '../reducers/cards'
 import { Card, Container, Modal } from 'semantic-ui-react'
 
 class Category extends React.Component {
+  state = { userAnswer: '' }
 
   componentDidMount() {
     const {id} = this.props
     this.props.dispatch(getCards(id))
+  }
+
+  handleClickA = () => {
+    this.setState({ userAnswer: 'a' })
+  }
+
+  handleClickB = () => {
+    this.setState({ userAnswer: 'b' })
+  }
+
+  handleClickC = () => {
+    this.setState({ userAnswer: 'c' })
+  }
+
+  handleClickD = () => {
+    this.setState({ userAnswer: 'd' })
+  }
+
+  answerCheck = () => {
+    
   }
 
   render() {
@@ -26,10 +47,38 @@ class Category extends React.Component {
               <Modal.Description>
                 <ul>
                   <h1>{card.question}</h1>
-                  <li><h2>A.{card.answer_a}</h2></li>
-                  <li><h2>B.{card.answer_b}</h2></li>
-                  <li><h2>C.{card.answer_c}</h2></li>
-                  <li><h2>D.{card.answer_d}</h2></li>
+                  <li>
+                    <h2 
+                      style={{cursor: "pointer"}} 
+                      onClick={this.handleClickA}
+                    >
+                      A.{card.answer_a}
+                    </h2>
+                  </li>
+                  <li>
+                    <h2 
+                      style={{cursor: "pointer"}} 
+                      onClick={this.handleClickB}
+                    >
+                      B.{card.answer_b}
+                    </h2>
+                  </li>
+                  <li>
+                    <h2 
+                      style={{cursor: "pointer"}} 
+                      onClick={this.handleClickC}
+                    >
+                      C.{card.answer_c}
+                    </h2>
+                  </li>
+                  <li>
+                    <h2 
+                      style={{cursor: "pointer"}} 
+                      onClick={this.handleClickD}
+                    >
+                      D.{card.answer_d}
+                    </h2>
+                  </li>
                 </ul>
               </Modal.Description>
             </Modal.Content>
