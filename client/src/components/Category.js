b6f57fddbb8f1829b4b7af3f176ede3a1a5ca900
Geoff Vincent <geoffkvincent@gmail.com>
2019-02-02 18:18:36 -0700

    clear state when modal closes

--- client/src/components/Category.js
+++ client/src/components/Category.js
@@ -64,7 +64,7 @@ class Category extends React.Component {
                   <h1>{card.points}</h1>
                 </Card.Content>
               }
-              open={this.state.open}
+              // open={this.state.open}
               onOpen={this.open}
               onClose={this.close}
             >
