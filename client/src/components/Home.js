b70be96daff58f856adfa00b34f344b13782d43e
Geoff Vincent <geoffkvincent@gmail.com>
2019-02-04 10:07:13 -0700

    new game

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Container, Header, Card, Button } from 'semantic-ui-react'
+import GameForm from './GameForm'
 import styled from 'styled-components'
 
 class Home extends React.Component {
+
+
   render() {
     return (
       <Container>
         <Header as='h1' textAlign='center'>React Trivia</Header>
         <Button>Create Game</Button>
+        <GameForm />
       </Container>
     )
   }
