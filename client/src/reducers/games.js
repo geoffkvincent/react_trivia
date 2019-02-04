fa7d4694a75d96e79df160b233e513965daa2413
Geoff Vincent <geoffkvincent@gmail.com>
2019-02-04 10:40:00 -0700

    new game reducer and actions

--- client/src/reducers/games.js
+++ client/src/reducers/games.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 const GAMES = 'GAMES'
-const ADD_GAME = 'ADDGAME'
+const ADD_GAME = 'ADD_GAME'
+const UPDATE_GAME = 'UPDATE_GAME'
+const DELETE_GAME = 'DELETE_GAME'
 
 export const getGames = () => {
   return (dispatch) => {
@@ -17,9 +19,35 @@ export const addGame = (game) => {
   }
 }
 
+export const updateGame = (game) => {
+  return (dispatch) => {
+    axios.get(`/api/games/${game.id}`, {game} )
+    .then( ({data}) => dispatch({ type: UPDATE_GAME, game: data }))
+  }
+}
+
+export const deleteGame = (id) => {
+  return (dispatch) => {
+    axios.get(`/api/games/${id}`)
+    .then( ({data}) => dispatch({ type: DELETE_GAME, id }))
+  }
+}
+
 export default ( state = [], action) => {
   switch(action.type) {
     case GAMES:
-    
+      return action.games
+    case ADD_GAME:
+      return [action.game, ...state]
+    case UPDATE_GAME:
+      return state.map( g => {
+        if(action.game.id === g.id)
+          return action.game
+        return g
+      })
+    case DELETE_GAME:
+      return state.filter(g => g.id !== action.id)
+    default:
+      return state
   }
 }
