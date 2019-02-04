17e19c6b02c01820104ff53b89d859dc015609ed
Geoff Vincent <geoffkvincent@gmail.com>
2019-02-04 10:44:10 -0700

    new game form

---
+++ client/src/components/GameForm.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Form } from 'semantic-ui-react'
+
+class GameForm extends React.Component {
+  state = { name: '' }
+
+  handleChange = (e) => {
+
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  render() {
+    return (
+      <Form>
+
+      </Form>
+    )
+  }
+}
+
+export default GameForm
