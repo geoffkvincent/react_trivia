4cfcb4f4bf0c7d04e2bd708076457b7d6e0c9822
Geoff Vincent <geoffkvincent@gmail.com>
2019-02-07 16:16:30 -0700

    fetch game

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom'
 import { Container } from 'semantic-ui-react'
-import FetchGame from 'semantic-ui-react'
+import FetchGame from './components/FetchGame'
 
 class App extends React.Component {
   render() {
     return (
      <Container>
        <Switch>
-         <Route exact path= '/' component={FetchGame} />
+         <Route path= '/' component={FetchGame} />
        </Switch>
      </Container>
     );
--- client/src/components/FetchGame.js
+++ client/src/components/FetchGame.js
@@ -1,25 +1,30 @@
 import React from 'react'
 import Home from './Home'
 import GameBoard from './GameBoard'
+import { getGames } from '../reducers/games'
+import { Switch, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
 import { Loader, } from 'semantic-ui-react'
 
 class FetchGame extends React.Component {
   state = { loaded: false }
 
-  
+  componentDidMount() {
+    
+  }
+
+  setLoaded = () => this.setState({loaded: true})
 
   render() {
     return (
-     <Container>
        <Switch>
          <Route exact path= '/' component={Home} />
          <Route exact path= '/games/:id' component={GameBoard}/>
          <Route render={() => <h1 className='text-center'>Four oh Four.</h1>} />
        </Switch>
-     </Container>
     );
   }
 
 }
 
-export default FetchGame
+export default connect()(FetchGame)
