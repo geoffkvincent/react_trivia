3a32195a21e009ff6d80bcf0d7fe244ab722d761
Geoff Vincent <geoffkvincent@gmail.com>
2019-02-07 16:42:06 -0700

    fetch game

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,17 +1,11 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import { Link } from 'react-router-dom'
-import { getGames } from '../reducers/games'
 import { Container, Header, Card, Button } from 'semantic-ui-react'
 import GameForm from './GameForm'
-import styled from 'styled-components'
 
 class Home extends React.Component {
   state = { showForm: false }
-
-  componentDidMount(){
-    this.props.dispatch(getGames())
-  }
 
   toggleForm = () => this.setState({ showForm: !this.state.showForm})
 
